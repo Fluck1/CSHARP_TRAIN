c1e90f24e34abf5b3167b56c2305ff05f611991e
Nikolay Kuchin <kuchinnikolay2@mail.ru>
2021-09-01 19:23:37 +0300

    123

---
+++ variables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLIGHT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");// вывод
+            //Console.ReadKey();//ввод клавиши
+            string name = "";
+            ushort age = 19;
+            long big_number = 366664;
+            uint year;
+            float average_score = 1.2f;
+            char symbol = 'h';
+            bool flag;
+            byte state = 2;
+            int number = 1000;
+            double quotient;
+
+
+
+        }
+    }
+}
